e5f743177f4167865958ab6e4fc88cfdeed1dc85
Bibhash Roy <bibash.roy@whitepeaksoft.com>
2016-11-07 11:17:47 +0530

    Initial commit

---
+++ app/src/main/java/com/timbuchalka/flickrbrowser/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.timbuchalka.flickrbrowser;
+
+import android.os.Bundle;
+
+public class SearchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        activateToolbar(true);
+    }
+
+}
